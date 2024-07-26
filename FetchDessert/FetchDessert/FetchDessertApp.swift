90b31a12ecc36004131c86419a067939d8338d8e
ReggieSean <yxuhuang@bu.edu>
2024-07-26 15:30:51 -0400

    first commit with custom package setup

---
+++ FetchDessert/FetchDessert/FetchDessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertApp.swift
+//  FetchDessert
+//
+//  Created by SeanHuang on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
